5f03b5cd5bc70dfa80ae06fca3af80e1ef786d81
bepsvpt <8221099+bepsvpt@users.noreply.github.com>
2023-09-23 19:59:44 +0800

    update phpdoc for facade

--- src/Facades/Webflow.php
+++ src/Facades/Webflow.php
@@ -11,10 +11,16 @@ use Storipress\Webflow\Requests\Item;
 use Storipress\Webflow\Requests\Site;
 
 /**
- * @method static Site site()
- * @method static Collection collection()
- * @method static CollectionField collectionField()
- * @method static Item item()
+ * @property-read Site $site
+ * @property-read Collection $collection
+ * @property-read CollectionField $collectionField
+ * @property-read Item $item
+ * @property-read string $token
+ * @property-read string $siteId
+ * @property-read int $retryAfter
+ * @property-read int $rateLimitRemaining
+ * @method static static setSiteId()
+ * @method static static setAccessToken()
  */
 class Webflow extends Facade
 {
