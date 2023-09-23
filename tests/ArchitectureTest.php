ed7a47da2864bf4f11ded1ec116657f3f27321ae
bepsvpt <8221099+bepsvpt@users.noreply.github.com>
2023-09-23 17:21:13 +0800

    add more architecture test and add type coverage test

--- tests/ArchitectureTest.php
+++ tests/ArchitectureTest.php
@@ -4,26 +4,31 @@ use Storipress\Webflow\Exceptions\Exception;
 use Storipress\Webflow\Objects\WebflowObject;
 use Storipress\Webflow\Requests\Request;
 
-it('must use strict type')
+test('There are no debugging statements remaining in our code.')
+    ->expect(['dd', 'dump', 'ray', 'var_dump', 'echo'])
+    ->not
+    ->toBeUsed();
+
+test('Strict typing must be enforced in the code.')
     ->expect('Storipress\Webflow')
     ->toUseStrictTypes();
 
-it('must not use final')
+test('The code should not utilize the "final" keyword.')
     ->expect('Storipress\Webflow')
     ->not()
     ->toBeFinal();
 
-it('must extend Request for Requests')
+test('All Request classes should extend "Request".')
     ->expect('Storipress\Webflow\Requests')
     ->classes()
     ->toExtend(Request::class);
 
-it('must extend WebflowObject for Objects')
+test('All Object classes should extend "WebflowObject".')
     ->expect('Storipress\Webflow\Objects')
     ->classes()
     ->toExtend(WebflowObject::class);
 
-it('must extend Exception for Exceptions')
+test('All Exception classes should extend "Exception".')
     ->expect('Storipress\Webflow\Exceptions')
     ->classes()
     ->toExtend(Exception::class);
